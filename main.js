4721953bc9b532a8c0c8c3be5de31db777c8163a
honziktillu <till@spsmb.cz>
2023-10-11 17:05:04 +0200

    super hot fixie fixie

--- main.js
+++ main.js
@@ -1,70 +1,75 @@
 const weights = document.getElementsByClassName("VHHlavicka");
-const stoleElement = document.getElementsByClassName('ig_Item igtbl_Item ig_10e26aa4_r1 ig_Header igtbl_Header ig_10e26aa4_r4')[1];
+const stoleElement = document.getElementsByClassName(
+  "ig_Item igtbl_Item ig_10e26aa4_r1 ig_Header igtbl_Header ig_10e26aa4_r4"
+)[1];
 
 window.onload = () => {
-    const weightValues = getWeightValues();
-    getResults(weightValues, stoleElement);
-}
+  const weightValues = getWeightValues();
+  getResults(weightValues, stoleElement);
+};
 
 const getWeightValues = () => {
-    let values = [];
-    [...weights].map((weight) => {
-        if (weight.title.includes("Váha")) {
-            let foo = weight.title;
-            foo = foo.replaceAll("Váha", "");
-            foo = foo.replaceAll("[", "");
-            foo = foo.replaceAll("]", "");
-            foo = foo.replaceAll(",", ".");
-            values.push(parseFloat(foo));
-        }
-    })
-    return values;
-}
+  let values = [];
+  [...weights].map((weight) => {
+    if (weight.title.includes("Váha")) {
+      let foo = weight.title;
+      foo = foo.replaceAll("Váha", "");
+      foo = foo.replaceAll("[", "");
+      foo = foo.replaceAll("]", "");
+      foo = foo.replaceAll(",", ".");
+      values.push(parseFloat(foo));
+    }
+  });
+  return values;
+};
 
 const getResults = (weightValues, table) => {
-    let foo = 0;
-    const rows = table.getElementsByTagName("tr");
-    [...rows].map((row) => {
-        foo++;
-        let averageIndex = 0;
-        const averages = row.getElementsByClassName("VHDilciPrumer");
-        let averageArray = [];
-        [...averages].map((item) => {
-            if (!item.previousElementSibling.hasAttribute("title")) {
-                averageIndex++;
-                return;
-            }
-            let data = [];
-            let itemTitle = item.previousElementSibling.title;
-            itemTitle = itemTitle.replaceAll(",", ".");
-            console.log(itemTitle);
-            if (itemTitle.includes(" ")) {
-                itemTitle = itemTitle.split(" ");
-                data.push(...itemTitle);
-            } else {
-                data.push(itemTitle);
-            }
-            averageArray.push({"index": averageIndex, "value": data});
-            averageIndex++;
-        });
-        if (!averageArray.length) return;
-        const averageResult = calculateAverages(weightValues, averageArray);
-        [...rows[foo - 1].getElementsByTagName("td")][0].innerHTML += `<p style="color: red; font-weight: bold;">Průměr: ${averageResult}</p>`;
+  let foo = 0;
+  const rows = table.getElementsByTagName("tr");
+  [...rows].map((row) => {
+    foo++;
+    let averageIndex = 0;
+    const averages = row.getElementsByClassName("VHDilciPrumer");
+    let averageArray = [];
+    [...averages].map((item) => {
+      if (!item.previousElementSibling.hasAttribute("title")) {
+        averageIndex++;
+        return;
+      }
+      let data = [];
+      let itemTitle = item.previousElementSibling.title;
+      itemTitle = itemTitle.replaceAll(",", ".");
+      console.log(itemTitle);
+      if (itemTitle.includes(" ")) {
+        itemTitle = itemTitle.split(" ");
+        data.push(...itemTitle);
+      } else {
+        data.push(itemTitle);
+      }
+      averageArray.push({ index: averageIndex, value: data });
+      averageIndex++;
     });
-}
+    if (!averageArray.length) return;
+    const averageResult = calculateAverages(weightValues, averageArray);
+    [
+      ...rows[foo - 1].getElementsByTagName("td"),
+    ][0].innerHTML += `<p style="color: red; font-weight: bold;">Průměr: ${averageResult}</p>`;
+  });
+};
 
 const calculateAverages = (weightValues, array) => {
-    let foo = 0;
-    let fooWeights = 0;
-    array.map((item) => {
-        console.log(item);
-        [...item.value].map((mark) => {
-            let fee = mark * weightValues[item.index];
-            foo += fee;
-            fooWeights += weightValues[item.index];
-        });
+  let foo = 0;
+  let fooWeights = 0;
+  array.map((item) => {
+    console.log(item);
+    [...item.value].map((mark) => {
+      if (isNaN(mark)) return;
+      let fee = mark * weightValues[item.index];
+      foo += fee;
+      fooWeights += weightValues[item.index];
     });
-    console.log(foo);
-    console.log(fooWeights);
-    return (foo / fooWeights).toFixed(2);
-}
+  });
+  console.log(foo);
+  console.log(fooWeights);
+  return (foo / fooWeights).toFixed(2);
+};
